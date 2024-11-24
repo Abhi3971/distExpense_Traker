e3bd8a21d907f5fc5b82e3e9268525320b9cb5e4
Abhi3971 <111583585+Abhi3971@users.noreply.github.com>
2024-11-24 16:48:45 +0530

    Update connectDb.js

--- Backend/config/connectDb.js
+++ Backend/config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDb = async() => {
     try{
-        await mongoose.connect(process.env.MONGO_URl)
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("connection established")
     }catch(error){
         console.log(`${error}`.bgRed)
